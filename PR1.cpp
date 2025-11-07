c40936246eb5fe4d8f5b56fff94aed8f1dd27410
Okidama <alex25kom@gmail.com>
2025-11-08 00:24:36 +0300

    Add files via upload

---
+++ PR1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	setlocale(0, "");
+	
+	int intt1 = 10;
+	int intt2 = 13;
+	int intt3 = 643;
+	int intt4 = 5432;
+	int intt5 = 425;
+	cout << "intt1 = " << intt1 << " | тип: int, размер: 4 б." << endl;
+	cout << "intt2 = " << intt2 << " | тип: int, размер: 4 б." << endl;
+	cout << "intt3 = " << intt3 << " | тип: int, размер: 4 б." << endl;
+	cout << "intt4 = " << intt4 << " | тип: int, размер: 4 б." << endl;
+	cout << "intt5 = " << intt5 << " | тип: int, размер: 4 б." << endl;
+	cout << endl;
+	
+	short shor1 = -342;
+	short shor2 = 543;
+	short shor3 = 6754;
+	short shor4 = 27524;
+	short shor5 = -657;
+	cout << "shor1 = " << shor1 << " | тип: short, размер: 2 б." << endl;
+	cout << "shor2 = " << shor2 << " | тип: short, размер: 2 б." << endl;
+	cout << "shor3 = " << shor3 << " | тип: short, размер: 2 б." << endl;
+	cout << "shor4 = " << shor4 << " | тип: short, размер: 2 б." << endl;
+	cout << "shor5 = " << shor5 << " | тип: short, размер: 2 б." << endl;
+	cout << endl;
+
+	double doub1 = 56527574;
+	double doub2 = 43254;
+	double doub3 = -543753;
+	double doub4 = 2354316;
+	double doub5 = 21556843;
+	cout << "doub1 = " << doub1 << " | тип: double, размер: 8 б." << endl;
+	cout << "doub2 = " << doub2 << " | тип: double, размер: 8 б." << endl;
+	cout << "doub3 = " << doub3 << " | тип: double, размер: 8 б." << endl;
+	cout << "doub4 = " << doub4 << " | тип: double, размер: 8 б." << endl;
+	cout << "doub5 = " << doub5 << " | тип: double, размер: 8 б." << endl;
+	cout << endl;
+
+	float fl1 = 34.63;
+	float fl2 = 2351.7643;
+	float fl3 = 1453.52;
+	float fl4 = -5446.32;
+	float fl5 = 34653.53265;
+	cout << "fl1 = " << fl1 << " | тип: float, размер: 4 б." << endl;
+	cout << "fl2 = " << fl2 << " | тип: float, размер: 4 б." << endl;
+	cout << "fl3 = " << fl3 << " | тип: float, размер: 4 б." << endl;
+	cout << "fl4 = " << fl4 << " | тип: float, размер: 4 б." << endl;
+	cout << "fl5 = " << fl5 << " | тип: float, размер: 4 б." << endl;
+	cout << endl;
+
+	string str1 = "res";
+	string str2 = "sss";
+	string str3 = "www";
+	string str4 = "asdf";
+	string str5 = "FDSA";
+	cout << "str1 = " << str1 << " | тип: string" << endl;
+	cout << "str2 = " << str2 << " | тип: string" << endl;
+	cout << "str3 = " << str3 << " | тип: string" << endl;
+	cout << "str4 = " << str4 << " | тип: string" << endl;
+	cout << "str5 = " << str5 << " | тип: string" << endl;
+	cout << endl;
+
+	char chr1 = 'a';
+	char chr2 = 'b';
+	char chr3 = 'c';
+	char chr4 = 'd';
+	char chr5 = 'e';
+	cout << "chr1 = " << chr1 << " | тип: char, размер: 1 б." << endl;
+	cout << "chr2 = " << chr2 << " | тип: char, размер: 1 б." << endl;
+	cout << "chr3 = " << chr3 << " | тип: char, размер: 1 б." << endl;
+	cout << "chr4 = " << chr4 << " | тип: char, размер: 1 б." << endl;
+	cout << "chr5 = " << chr5 << " | тип: char, размер: 1 б." << endl;
+	cout << endl;
+
+	bool b1 = true;
+	bool b2 = false;
+	cout << "b1 = " << b1 << " | тип: bool, размер: 1 б." << endl;
+	cout << "b2 = " << b2 << " | тип: bool, размер: 1 б." << endl;
+}
